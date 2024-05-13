2f426c61d605a84b3e94a4f85f5c0e151e66526c
Christopher Zenzel <christopher@arendellekingdom.com>
2024-05-13 17:25:43 -0400

    Update the project for permissions requests

---
+++ 03-Requesting-Permissions/Octo.BluetoothApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Octo.BluetoothApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
